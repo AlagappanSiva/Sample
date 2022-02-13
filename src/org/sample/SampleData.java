52265e3d49b8f9507b26e35de6567de34d451c87
AlagappanSiva <alagappan181099@gmail.com>
2022-02-13 13:23:44 +0530

    done by coding

--- src/org/sample/SampleData.java
+++ src/org/sample/SampleData.java
@@ -5,12 +5,12 @@ public class SampleData {
 	public static void main(String[] args) {
 		System.out.println("Hi...");
 		
-<<<<<<< HEAD
+		
 		System.out.println("12345");
-=======
+
 		
 		System.out.println("bye...");
->>>>>>> 24f2e7a1a53bf5245ce0c729f65732f3d253b6e7
+
 	}
 	
 	
